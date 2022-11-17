c47bd799d208655087b713027f1558926d2d03bc
fianmuhamad <alfiansaniputra77@gmail.com>
2022-11-17 14:17:10 +0700

    WIP Assignment number 1

---
+++ Day 2/index.js
@@ -0,0 +1,7 @@
+    const a=5;
+    const b=10
+    var c=a*b;
+    
+    console.log("Konstanta Nilai A =" +a+ " (tidak bisa diubah krn tipe var=const)");
+    console.log("Konstanta Nilai B = "+b+ " (tidak bisa diubah krn tipe var=const)");
+    console.log("hasil perkalian "+a+" * "+b+" = " + c);
